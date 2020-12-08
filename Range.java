c6432d4b414db393f9f1f70e0acc83148d81c951
Joshua Kloepfer <jkloepfer20@stuy.edu>
2020-12-07 22:48:09 -0500

    i have completed the class

---
+++ Range.java
@@ -0,0 +1,27 @@
+import java.util.NoSuchElementException;
+public class Range implements IntegerSequence {
+  private int start, end, current;
+  public Range(int start, int end) {
+    this.start = start;
+    this.end = end;
+    this.current = start;
+  }
+  public void reset() {
+    current = start;
+  }
+  public int length() {
+    return end - start + 1;
+  }
+  public boolean hasNext() {
+    return current < end;
+  }
+  public int next() {
+    if (hasNext()) {
+      current += 1;
+      return current - 1;
+    }
+    else {
+      throw new NoSuchElementException();
+    }
+  }
+}
